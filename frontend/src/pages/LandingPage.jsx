38a11a308e4a8d0b80026da357edce7e848431cf
meritikprasad <ritikprasad1510@gmail.com>
2025-12-25 20:39:29 +0530

    checking git

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,6 +2,6 @@ import "./LandingPage.css"
 
 export default function LandingPage() {
     return (
-        <div>LandingPage</div>
+        <div className="LandingPageContainer">LandingPage</div>
     )
 }
